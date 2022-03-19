02ff9bbfa3a7820f500d7d325aede7a4fca7b2ba
LemengDai <51035918+LemengDai@users.noreply.github.com>
2022-03-18 21:13:20 -0400

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -29,24 +29,12 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: const Color.fromARGB(255, 236, 220, 226),
-          onTap: onTap,
-          currentIndex: currentIndex,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black.withOpacity(0.5),
-          elevation: 0,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(
-                label: 'My Doctors', icon: Icon(Icons.medical_services)),
-            BottomNavigationBarItem(
-                label: 'Community', icon: Icon(Icons.forum)),
-            BottomNavigationBarItem(label: 'Me', icon: Icon(Icons.person)),
-          ]),
+      bottomNavigationBar: BottomNavigationBar(type: BottomNavigationBarType.fixed, backgroundColor: Colors.pink[50], onTap: onTap, currentIndex: currentIndex, selectedItemColor: Colors.black, unselectedItemColor: Colors.black.withOpacity(0.5), elevation: 0, showSelectedLabels: false, showUnselectedLabels: false, items: const [
+        BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+        BottomNavigationBarItem(label: 'My Doctors', icon: Icon(Icons.medical_services)),
+        BottomNavigationBarItem(label: 'Community', icon: Icon(Icons.forum)),
+        BottomNavigationBarItem(label: 'Me', icon: Icon(Icons.person)),
+      ]),
     );
   }
 }
